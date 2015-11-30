a2973eabc8d77f7d9c6f08028ed65943d660f0f8
Tiago Katcipis <tiagokatcipis@gmail.com>
2015-11-29 23:29:26 -0200

    example was a little odd

--- doc.go
+++ doc.go
@@ -2,17 +2,15 @@
 
 testd that makes easier to write integration tests that depends on runnings daemons/services.
 
-Example:
+Create a Testd instance, informing where you want the logs to be saved,
+the command to execute and its arguments.
 
-  // Create a Testd instance, informing where you want the logs to be saved,
-  // the command to execute and its arguments.
-  //
   daemon, err := testd.New("./tests/debug/daemon.log", "daemon", "arg1", "arg2")
 
-  // Do your testing, that depends on the daemon being running.
-  // When you are done testing, just call the Stop method.
+Do your testing, that depends on the daemon being running.
+When you are done testing, just call the Stop method.
   err := daemon.Stop()
 
-  // Remember to do proper error handling, both on New and Stop.
+Remember to do proper error handling, both on New and Stop.
 */
 package testd
